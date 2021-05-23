f6bc3ce4c04dbd1f9beb829af29dfb6dcbc9a145
autsada <autsada.t@gmail.com>
2021-05-23 20:05:49 +0700

    Redux-thunk starter

--- src/data/users.js
+++ src/data/users.js
@@ -2,11 +2,29 @@ export const users = [
   {
     id: 'a',
     username: 'Somsak',
+    email: 'somsak@test.com',
     password: 'abc123',
   },
   {
     id: 'b',
     username: 'Wanchai',
+    email: 'wanchai@test.com',
     password: '123abc',
   },
 ]
+
+export function signin(email, password) {
+  return new Promise((resolve, reject) => {
+    const foundUser = users.find(
+      (user) => user.email === email && user.password === password
+    )
+
+    setTimeout(() => {
+      if (foundUser) {
+        resolve(foundUser)
+      } else {
+        reject('Email or password is invalid')
+      }
+    }, 3000)
+  })
+}
